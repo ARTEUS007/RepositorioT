2adb6e811fc310387098810347aceea83dd44b38
Igor Borba Fraga <igor_fraga@estudante.sc.senai.br>
2021-04-29 09:23:04 -0300

    primeiro comit

---
+++ src/sample/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Conta conta = new Conta();
+        conta.setNumAgencia(19999);
+        conta.setNumConta(123);
+        conta.depositar(100);
+        conta.sacar(25);
+        System.out.println(conta);
+
+    }
+}
