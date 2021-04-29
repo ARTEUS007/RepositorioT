33841687f26d47b419a1501f016a58df8dbebd65
ARTEUS007 <72563657+ARTEUS007@users.noreply.github.com>
2021-04-29 10:27:02 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,30 +1,23 @@
 package sample;
 
-import sample.model.Conta;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main  {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
 
     public static void main(String[] args) {
-
-        Conta conta1 = new Conta(5678,
-                123456);
-        System.out.println(conta1);
-
-        if (conta1.sacar(200)){
-            System.out.println("Sacou R$200,00 ");
-        } else {
-            System.out.println("Não tem limite");
-        }
-        System.out.println(conta1);
-
-        conta1.depositar(300);
-        //conta1.depositar(200);
-
-        System.out.println(conta1);
-
-
-
-
-
+        launch(args);
     }
 }
